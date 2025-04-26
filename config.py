92e17d092a996e3ec5cbf510882fcbdca6a7d667
viratcpu <vishalrahul964@gmail.com>
2025-04-26 19:39:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
+api_id = "26468828"
+api_hash = "4693513c08d1ac6af15f95b116c29478"
 bot_token = "7768725383:AAFOaTqDmuFQw2dAdXlgJH-O760qYfDZyc8"
-auth_users = [5840594311]
+auth_users = [794443074]
